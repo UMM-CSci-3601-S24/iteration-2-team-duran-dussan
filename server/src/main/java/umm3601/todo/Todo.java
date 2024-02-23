7a3a4885057c1f336d6efe1d958e845874348e36
Ely Johnson <elyjohnson6@gmail.com>
2024-02-23 12:32:39 -0600

    Added variables to todo class such as id, name, etc.

--- server/src/main/java/umm3601/todo/Todo.java
+++ server/src/main/java/umm3601/todo/Todo.java
@@ -1,5 +1,17 @@
 package umm3601.todo;
 
+import org.mongojack.Id;
+import org.mongojack.ObjectId;
+
+@SuppressWarnings({"VisibilityModifier"})
 public class Todo {
 
+    @ObjectId @Id
+    @SuppressWarnings({"MemberName"})
+    public String _id;
+
+    public String name;
+    public String userName;
+    public String email;
+    
 }

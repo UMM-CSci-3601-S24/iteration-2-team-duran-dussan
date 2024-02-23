ea01d6c588eb214c17ccc279a830da4e93cc26cf
Ely Johnson <elyjohnson6@gmail.com>
2024-02-23 12:38:17 -0600

    Added variables to Task.java such as Id, name, and status

--- server/src/main/java/umm3601/todo/Task.java
+++ server/src/main/java/umm3601/todo/Task.java
@@ -1,5 +1,15 @@
 package umm3601.todo;
 
+import org.mongojack.Id;
+import org.mongojack.ObjectId;
+
+@SuppressWarnings({"VisibilityModifier"})
 public class Task {
 
+    @ObjectId @Id
+    @SuppressWarnings({"MemberName"})
+    public String _id;
+
+    public String name;
+    public boolean status;
 }

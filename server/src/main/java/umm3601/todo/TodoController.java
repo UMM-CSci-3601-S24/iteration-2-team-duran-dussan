d24758f4d5a2cf9f108efffbd06b36b98250c511
Ely Johnson <elyjohnson6@gmail.com>
2024-02-23 12:47:28 -0600

    Added variables and addRoutes method to TodoController, but most is commented out because the variables are unused

--- server/src/main/java/umm3601/todo/TodoController.java
+++ server/src/main/java/umm3601/todo/TodoController.java
@@ -1,5 +1,33 @@
 package umm3601.todo;
 
-public class TodoController {
+import io.javalin.Javalin;
+import umm3601.Controller;
+
+// import org.bson.UuidRepresentation;
+// import org.mongojack.JacksonMongoCollection;
+
+// import com.mongodb.client.MongoDatabase;
+
+public class TodoController implements Controller{
+
+  // private static final String API_TODOS = "/api/todos";
+  // private static final String API_TODO_BY_ID = "/api/todos/{id}";
+  // private final String Todo_Key = "todoId";
+
+  // private final JacksonMongoCollection<Todo> todoCollection;
+
+  // public TodoController(MongoDatabase database) {
+  //   todoCollection = JacksonMongoCollection.builder().build(
+  //     database,
+  //     "todos",
+  //     Todo.class,
+  //      UuidRepresentation.STANDARD);
+  // }
+
+  @Override
+  public void addRoutes(Javalin server) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'addRoutes'");
+  }
 
 }

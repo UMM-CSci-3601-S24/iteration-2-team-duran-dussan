a28f4fcedcf79ca0ed2bdd08eaaaf8796fa22634
Ely Johnson <elyjohnson6@gmail.com>
2024-02-24 22:37:13 -0600

    Implemented constructFilter and constructSortingOrder methods and added huntCollection to the TodoController

--- server/src/main/java/umm3601/todo/TodoController.java
+++ server/src/main/java/umm3601/todo/TodoController.java
@@ -2,15 +2,18 @@ package umm3601.todo;
 
 import io.javalin.Javalin;
 import io.javalin.http.Context;
+import io.javalin.http.HttpStatus;
 import umm3601.Controller;
 
 import java.util.ArrayList;
 
+import org.bson.Document;
 import org.bson.UuidRepresentation;
 import org.bson.conversions.Bson;
 import org.mongojack.JacksonMongoCollection;
 
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Sorts;
 
 public class TodoController implements Controller {
 
@@ -20,6 +23,7 @@ public class TodoController implements Controller {
   private static final String API_HUNTS = "/api/hunts";
 
   private final JacksonMongoCollection<Todo> todoCollection;
+  private final JacksonMongoCollection<Hunt> huntCollection;
 
   public TodoController(MongoDatabase database) {
     todoCollection = JacksonMongoCollection.builder().build(
@@ -27,26 +31,34 @@ public class TodoController implements Controller {
       "todos",
       Todo.class,
        UuidRepresentation.STANDARD);
+    huntCollection = JacksonMongoCollection.builder().build(
+      database,
+      "hunts",
+      Hunt.class,
+       UuidRepresentation.STANDARD);
   }
 
   public void getHunts(Context ctx) {
     Bson combinedFilter = constructFilter(ctx);
     Bson sortingOrder = constructSortingOrder(ctx);
 
-    ArrayList<Todo> matchingTodos = todoCollection
+    ArrayList<Hunt> matchingHunts = huntCollection
       .find(combinedFilter)
       .sort(sortingOrder)
       .into(new ArrayList<>());
+
+      ctx.json(matchingHunts);
+      ctx.status(HttpStatus.OK);
   }
 
   private Bson constructSortingOrder(Context ctx) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'constructSortingOrder'");
+    Bson sortingOrder = Sorts.ascending("name");
+    return sortingOrder;
   }
 
   private Bson constructFilter(Context ctx) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'constructFilter'");
+    Bson filter = new Document();
+    return filter;
   }
 
   @Override

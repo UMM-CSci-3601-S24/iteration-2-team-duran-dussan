128d89da15a5e0761e79ec64110cce31f2ac2542
Ely Johnson <elyjohnson6@gmail.com>
2024-02-24 18:37:57 -0600

    Added before all, after all, and before each for todoContollerSpec tests

--- server/src/test/java/umm3601/todo/TodoControllerSpec.java
+++ server/src/test/java/umm3601/todo/TodoControllerSpec.java
@@ -1,5 +1,124 @@
 package umm3601.todo;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import org.bson.Document;
+import org.bson.types.ObjectId;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import com.mongodb.MongoClientSettings;
+import com.mongodb.ServerAddress;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
+import io.javalin.http.Context;
+import io.javalin.json.JavalinJackson;
+
+@SuppressWarnings({ "MagicNumber" })
 public class TodoControllerSpec {
 
+  private TodoController todoController;
+
+  private ObjectId frysId;
+
+  private static MongoClient mongoClient;
+  private static MongoDatabase db;
+  private static JavalinJackson javalinJackson = new JavalinJackson();
+
+  @Mock
+  private Context ctx;
+
+  @Captor
+  private ArgumentCaptor<ArrayList<Todo>> todoArrayListCaptor;
+
+  @Captor
+  private ArgumentCaptor<Todo> todoCaptor;
+
+  @Captor
+  private ArgumentCaptor<Map<String, String>> mapCaptor;
+
+  @BeforeAll
+  static void setupAll() {
+    String mongoAddr = System.getenv().getOrDefault("MONGO_ADDR", "localhost");
+
+    mongoClient = MongoClients.create(
+      MongoClientSettings.builder()
+        .applyToClusterSettings(builder -> builder.hosts(Arrays.asList(new ServerAddress(mongoAddr))))
+        .build());
+      db = mongoClient.getDatabase("test");
+  }
+
+  @AfterAll
+  static void teardown() {
+    db.drop();
+    mongoClient.close();
+  }
+
+  @BeforeEach
+  void setupEach() throws IOException {
+    MockitoAnnotations.openMocks(this);
+
+    MongoCollection<Document> todoDocuments = db.getCollection("todos");
+    todoDocuments.drop();
+    frysId = new ObjectId();
+    Document fry = new Document()
+      .append("_id", frysId)
+      .append("name", "Fry")
+      .append("userName", "fry")
+      .append("email", "fry@email");
+
+    todoDocuments.insertOne(fry);
+
+    MongoCollection<Document> huntDocuments = db.getCollection("hunts");
+    huntDocuments.drop();
+    List<Document> testHunts = new ArrayList<>();
+    testHunts.add(
+      new Document()
+        .append("todoId", frysId)
+        .append("name", "Fry's Hunt")
+        .append("description", "Fry's hunt for the seven leaf clover")
+        .append("est", 20)
+        .append("tasks", Arrays.asList(
+          new ObjectId(),
+          new ObjectId(),
+          new ObjectId()
+        )));
+    testHunts.add(
+      new Document()
+        .append("todoId", frysId)
+        .append("name", "Fry's Hunt 2")
+        .append("description", "Fry's hunt for Morris")
+        .append("est", 30)
+        .append("tasks", Arrays.asList(
+          new ObjectId(),
+          new ObjectId(),
+          new ObjectId()
+        )));
+    testHunts.add(
+      new Document()
+        .append("todoId", frysId)
+        .append("name", "Fry's Hunt 3")
+        .append("description", "Fry's hunt for money")
+        .append("est", 40)
+        .append("tasks", Arrays.asList(
+          new ObjectId(),
+          new ObjectId(),
+          new ObjectId()
+        )));
+    huntDocuments.insertMany(testHunts);
+
+    todoController = new TodoController(db);
+  }
 }

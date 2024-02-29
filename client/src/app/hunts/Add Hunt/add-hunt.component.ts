b6eed82b5ee59a56e0ef0fb6a47e3f459ba1cf93
Ely Johnson <elyjohnson6@gmail.com>
2024-02-29 10:27:18 -0600

    Added hunt validation messages

--- client/src/app/hunts/Add Hunt/add-hunt.component.ts
+++ client/src/app/hunts/Add Hunt/add-hunt.component.ts
@@ -21,15 +21,38 @@ export class AddHuntComponent {
     description: new FormControl('', Validators.compose([
       Validators.required,
       Validators.minLength(1),
-      Validators.maxLength(50)
+      Validators.maxLength(200)
     ])),
 
     est: new FormControl<number>(null, Validators.compose([
       Validators.required,
       Validators.min(0),
-      Validators.max(20),
+      Validators.max(240),
       Validators.pattern('^[0-9]+$')
     ])),
-  })};
+  });
+
+  readonly addHuntValidationMessages = {
+    name: [
+      { type: 'required', message: 'Name is required' },
+      { type: 'minlength', message: 'Name must be at least 1 character long' },
+      { type: 'maxlength', message: 'Name cannot be more than 50 characters long' }
+    ],
+
+    description: [
+      { type: 'required', message: 'Description is required' },
+      { type: 'minlength', message: 'Description must be at least 1 character long' },
+      { type: 'maxlength', message: 'Description cannot be more than 200 characters long' }
+    ],
+
+    est: [
+      { type: 'required', message: 'Estimated time is required' },
+      { type: 'min', message: 'Estimated time must be at least 0' },
+      { type: 'max', message: 'Estimated time cannot be more than 4 hours' },
+      { type: 'pattern', message: 'Estimated time must be a number' }
+    ]
+  }
+
+};
 
 

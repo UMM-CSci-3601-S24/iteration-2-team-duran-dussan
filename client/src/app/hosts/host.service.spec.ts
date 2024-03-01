8062a47858325032ca031904dd3a2fffc508f611
mahon479 <mahon479@morris.umn.edu>
2024-02-29 23:15:46 -0600

    Added tests for host.service

--- client/src/app/hosts/host.service.spec.ts
+++ client/src/app/hosts/host.service.spec.ts
@@ -1,29 +1,35 @@
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed, waitForAsync } from '@angular/core/testing';
 import { of } from 'rxjs';
-import { Host } from './host';
+import { Hunt } from '../hunts/hunt';
 import { HostService } from './host.service';
 
 describe('HostService', () => {
-const testHosts: Host[] = [
+const testHunts: Hunt[] = [
   {
-    _id: 'chris_id',
-    name: 'Chris',
-    userName: '17chrispy',
-    email: 'chris@this.that',
+    _id: "5889",
+    hostId: "588",
+    name: "Default Hunt 1",
+    description: "This is the default hunt 1",
+    est: 20,
+    numberOfTasks: 4
   },
   {
-    _id: 'tom_id',
-    name: 'Tom',
-    userName: 'tomtastic8',
-    email: 'tom@this.that',
+    _id: "5754",
+    hostId: "575",
+    name: "Default Hunt 2",
+    description: "This is the default hunt 2",
+    est: 17,
+    numberOfTasks: 4
   },
   {
-    _id: 'ann_id',
-    name: 'Ann',
-    userName: '$4ann',
-    email: 'ann@this.that',
+    _id: "de7c",
+    hostId: "e7c",
+    name: "Default Hunt 3",
+    description: "This is the default hunt 3",
+    est: 15,
+    numberOfTasks: 4
   },
 ];
 let hostService: HostService;
@@ -46,23 +52,45 @@ afterEach(() => {
   httpTestingController.verify();
 });
 
-describe('When getHosts() is called with no parameters', () => {
+describe('When getHunts() is called', () => {
 
-   it('calls `api/hosts`', waitForAsync(() => {
+  it('calls `api/hosts/575`', waitForAsync(() => {
 
-     const mockedMethod = spyOn(httpClient, 'get').and.returnValue(of(testHosts));
+    const mockedMethod = spyOn(httpClient, 'get').and.returnValue(of(testHunts));
 
-     hostService.getHosts().subscribe(() => {
+    hostService.getHunts("575").subscribe(() => {
 
+     expect(mockedMethod)
+      .withContext('one call')
+      .toHaveBeenCalledTimes(1);
+
+     expect(mockedMethod)
+      .withContext('talks to the correct endpoint')
+      .toHaveBeenCalledWith(`${hostService.hostUrl}/575`);
+    });
+  }));
+ });
+
+ describe('When getUserById() is given an ID', () => {
+
+   it('calls api/hosts/hostId with the correct hostId', waitForAsync(() => {
+
+     const targetHunt: Hunt = testHunts[1];
+     const targetId: string = targetHunt.hostId;
+
+     const mockedMethod = spyOn(httpClient, 'get').and.returnValue(of(targetHunt));
+
+     hostService.getHuntById(targetId).subscribe(() => {
        expect(mockedMethod)
          .withContext('one call')
          .toHaveBeenCalledTimes(1);
-
        expect(mockedMethod)
          .withContext('talks to the correct endpoint')
-         .toHaveBeenCalledWith(hostService.hostUrl, { params: new HttpParams() });
+         .toHaveBeenCalledWith(`${hostService.huntUrl}/${targetId}`);
      });
    }));
  });
+
+
 
 })

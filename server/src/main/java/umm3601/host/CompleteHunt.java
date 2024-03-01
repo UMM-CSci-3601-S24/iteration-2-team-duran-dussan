f45770a6367a5f45936acee0927d5db3ff32ee95
Ely Johnson <elyjohnson6@gmail.com>
2024-03-01 15:40:40 -0600

    Added CompleteHunt class that combines a hunt with its tasks

---
+++ server/src/main/java/umm3601/host/CompleteHunt.java
@@ -0,0 +1,9 @@
+package umm3601.host;
+
+import java.util.ArrayList;
+
+@SuppressWarnings({"VisibilityModifier"})
+public class CompleteHunt {
+  public Hunt hunt;
+  public ArrayList<Task> tasks;
+}

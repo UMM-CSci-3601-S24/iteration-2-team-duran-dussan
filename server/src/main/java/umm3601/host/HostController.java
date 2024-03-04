1d052531495a203bec284c7062283c35aca7a77b
Ely Johnson <elyjohnson6@gmail.com>
2024-03-04 17:44:52 -0600

    Added method for deleting a task

--- server/src/main/java/umm3601/host/HostController.java
+++ server/src/main/java/umm3601/host/HostController.java
@@ -189,6 +189,19 @@ public class HostController implements Controller {
     ctx.status(HttpStatus.OK);
   }
 
+  public void deleteTask(Context ctx) {
+    String id = ctx.pathParam("id");
+    DeleteResult deleteResult = taskCollection.deleteOne(eq("_id", new ObjectId(id)));
+    if (deleteResult.getDeletedCount() != 1) {
+      ctx.status(HttpStatus.NOT_FOUND);
+      throw new NotFoundResponse(
+        "Was unable to delete ID "
+          + id
+          + "; perhaps illegal ID or an ID for an item not in the system?");
+    }
+    ctx.status(HttpStatus.OK);
+  }
+
   public void getCompleteHunt(Context ctx) {
     CompleteHunt completeHunt = new CompleteHunt();
     completeHunt.hunt = getHunt(ctx);

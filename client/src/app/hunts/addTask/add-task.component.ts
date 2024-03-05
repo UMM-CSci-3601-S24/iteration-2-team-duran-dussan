3232ead699971159b7d99fd68d53faa4a01a4d05
Ely Johnson <elyjohnson6@gmail.com>
2024-03-05 14:01:40 -0600

    Changed window reload to delay so add task message is displayed

--- client/src/app/hunts/addTask/add-task.component.ts
+++ client/src/app/hunts/addTask/add-task.component.ts
@@ -70,6 +70,7 @@ export class AddTaskComponent {
           null,
           { duration: 2000 }
         );
+        setTimeout(() => window.location.reload(), 2000);
       },
       error: err => {
         this.snackBar.open(
@@ -79,6 +80,5 @@ export class AddTaskComponent {
         );
       },
     });
-    window.location.reload();
   }
 }

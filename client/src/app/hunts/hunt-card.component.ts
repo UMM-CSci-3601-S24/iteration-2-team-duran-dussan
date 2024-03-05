525e1634f4076a2c43a32ae0c45e1d8bc0e4c81b
Jaden Rainey <raine123@morris.umn.edu>
2024-03-05 13:19:10 -0600

    revamped hunt-profile

--- client/src/app/hunts/hunt-card.component.ts
+++ client/src/app/hunts/hunt-card.component.ts
@@ -1,22 +1,23 @@
-import { Component, input } from '@angular/core';
+import { Component, input, } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { RouterLink } from '@angular/router';
 import { Hunt } from './hunt';
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-hunt-card',
     templateUrl: './hunt-card.component.html',
     styleUrls: ['./hunt-card.component.scss'],
     standalone: true,
-    imports: [MatCardModule, MatButtonModule, MatListModule, MatIconModule, RouterLink]
+    imports: [CommonModule, MatCardModule, MatButtonModule, MatListModule, MatIconModule, RouterLink]
 })
 export class HuntCardComponent {
 
   hunt = input.required<Hunt>();
-  simple = input(false);
+  simple = input(true);
 }
 
 

75afd71bb30a13a44563e1eaf0fafd2551bfed3f
AndreasNelson <nel02254@morris.umn.edu>
2024-03-21 23:12:51 -0500

    added a code form field and a join hunt button

--- client/src/app/hunters/join-hunt/join-hunt.component.ts
+++ client/src/app/hunters/join-hunt/join-hunt.component.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-join-hunt',
   standalone: true,
-  imports: [],
+  imports: [
+    MatInputModule,
+    MatCardModule,
+    CommonModule,
+    MatButtonModule,
+    RouterModule],
   templateUrl: './join-hunt.component.html',
   styleUrl: './join-hunt.component.scss'
 })

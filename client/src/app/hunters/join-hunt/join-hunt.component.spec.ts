e6f864a985a8522844e15f080a2a694dbb9909a0
AndreasNelson <nel02254@morris.umn.edu>
2024-03-22 11:16:45 -0500

    added testing for access code text filtering to get coverage above 80%

--- client/src/app/hunters/join-hunt/join-hunt.component.spec.ts
+++ client/src/app/hunters/join-hunt/join-hunt.component.spec.ts
@@ -20,4 +20,35 @@ describe('JoinHuntComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  describe('numericOnly', () => {
+    it('should return true for numeric input', () => {
+      const event = {
+        which: 50, // represents '2'
+        preventDefault: jasmine.createSpy('preventDefault')
+      };
+      expect(component.numericOnly(event)).toBeTrue();
+      expect(event.preventDefault).not.toHaveBeenCalled();
+    });
+
+    it('should return false for non-numeric input', () => {
+      const event = {
+        which: 65, // represents 'A'
+        preventDefault: jasmine.createSpy('preventDefault')
+      };
+      expect(component.numericOnly(event)).toBeFalse();
+      expect(event.preventDefault).toHaveBeenCalled();
+    });
+
+    it('should use keyCode if which is not available', () => {
+      const event = {
+        keyCode: 50, // represents '2'
+        preventDefault: jasmine.createSpy('preventDefault')
+      };
+      expect(component.numericOnly(event)).toBeTrue();
+      expect(event.preventDefault).not.toHaveBeenCalled();
+    });
+  });
 });
+
+

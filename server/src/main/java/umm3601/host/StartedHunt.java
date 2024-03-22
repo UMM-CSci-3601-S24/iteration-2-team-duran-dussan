3b2da8cad4e40dd9c44c072ae2a30a5e1d83df82
Ken <brod0268@morris.umn.edu>
2024-03-21 22:14:59 -0500

    Created StartedHunt.java
    Need to code controller and tests

---
+++ server/src/main/java/umm3601/host/StartedHunt.java
@@ -0,0 +1,15 @@
+package umm3601.host;
+
+
+import org.mongojack.Id;
+import org.mongojack.ObjectId;
+
+@SuppressWarnings({"VisibilityModifier"})
+public class StartedHunt {
+
+  @ObjectId @Id
+  @SuppressWarnings({"MemberName"})
+    public String _id;
+    public String accessCode;
+    public Hunt hunt;
+}

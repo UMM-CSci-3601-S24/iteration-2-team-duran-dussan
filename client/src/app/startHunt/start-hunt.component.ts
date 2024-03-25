cc85d5d6287b1f6d01a5a6c87a33156740ced817
Ely Johnson <elyjohnson6@gmail.com>
2024-03-25 13:05:33 -0500

    Created StartHuntComponent export class to get a started hunt using an access code

--- client/src/app/startHunt/start-hunt.component.ts
+++ client/src/app/startHunt/start-hunt.component.ts
@@ -1,4 +1,10 @@
-import { Component } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { MatDialog } from "@angular/material/dialog";
+import { MatSnackBar } from "@angular/material/snack-bar";
+import { ActivatedRoute, Router, ParamMap } from "@angular/router";
+import { Subject, map, switchMap, takeUntil } from "rxjs";
+import { HostService } from "../hosts/host.service";
+import { StartedHunt } from "./startedHunt";
 
 @Component({
   selector: 'app-start-hunt-component',
@@ -9,6 +15,40 @@ import { Component } from "@angular/core";
   imports: []
 })
 
-export class StartHuntComponent {
-  
+export class StartHuntComponent implements OnInit, OnDestroy {
+  startedHunt: StartedHunt;
+  error: { help: string, httpResponse: string, message: string };
+
+  private ngUnsubscribe = new Subject<void>();
+
+  constructor(private snackBar: MatSnackBar, private route: ActivatedRoute, private hostService: HostService, private router: Router, public dialog: MatDialog) { }
+
+  ngOnInit(): void {
+
+    this.route.paramMap.pipe(
+
+      map((paramMap: ParamMap) => paramMap.get('accessCode')),
+
+      switchMap((accessCode: string) => this.hostService.getStartedHunt(accessCode)),
+
+      takeUntil(this.ngUnsubscribe)
+    ).subscribe({
+      next: startedHunt => {
+        this.startedHunt = startedHunt;
+        return ;
+      },
+      error: _err => {
+        this.error = {
+          help: 'There was a problem starting the hunt – try again.',
+          httpResponse: _err.message,
+          message: _err.error?.title,
+        };
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    this.ngUnsubscribe.next();
+    this.ngUnsubscribe.complete();
+  }
 }

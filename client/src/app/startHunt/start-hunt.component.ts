d5f8452fe17c6f66313a6d2345a0c4c3e8ed8c94
mahon479 <mahon479@morris.umn.edu>
2024-03-26 14:03:37 -0500

    Updated the access code page display

--- client/src/app/startHunt/start-hunt.component.ts
+++ client/src/app/startHunt/start-hunt.component.ts
@@ -5,6 +5,11 @@ import { ActivatedRoute, Router, ParamMap } from "@angular/router";
 import { Subject, map, switchMap, takeUntil } from "rxjs";
 import { HostService } from "../hosts/host.service";
 import { StartedHunt } from "./startedHunt";
+import { MatCardModule } from "@angular/material/card";
+import { HttpClientModule } from "@angular/common/http";
+import { MatIconButton } from "@angular/material/button";
+import { MatDivider } from "@angular/material/divider";
+import { MatIcon } from "@angular/material/icon";
 
 @Component({
   selector: 'app-start-hunt-component',
@@ -12,7 +17,7 @@ import { StartedHunt } from "./startedHunt";
   styleUrls: ['./start-hunt.component.scss'],
   providers: [],
   standalone: true,
-  imports: []
+  imports: [MatCardModule, MatDivider, MatIconButton, MatIcon, HttpClientModule]
 })
 
 export class StartHuntComponent implements OnInit, OnDestroy {

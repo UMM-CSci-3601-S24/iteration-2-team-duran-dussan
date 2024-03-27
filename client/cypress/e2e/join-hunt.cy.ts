fd0f506b80ee4e0ef786738f8a13e0c79442984e
Andrew Lam <lam00139@morris.umn.edu>
2024-03-27 13:12:04 -0500

    e2e testing displaying join hunt page component.

--- client/cypress/e2e/join-hunt.cy.ts
+++ client/cypress/e2e/join-hunt.cy.ts
@@ -6,6 +6,22 @@ describe('Join Hunt', () => {
   beforeEach(() => page.navigateTo());
 
   it('should display the right title for join hunt page', () => {
-    page.getJoinHuntTitle().contains('Join Hunt');
+    page.getJoinHuntTitle().contains('Type Invite Code');
   });
-})
+
+  it('should display the access code input field', () => {
+    page.getAccessCodeInput().should('exist');
+  });
+
+  it('should display the join hunt button', () => {
+    page.getJoinHuntButton().should('exist');
+    page.getJoinHuntButton().contains('Join Hunt');
+  });
+
+  // it('should display the join hunt button as disabled with no access code', () => {
+  //   page.getAccessCodeInput().should('exist');
+  //   page.getAccessCodeInput().should('have.value', '');
+  //   page.getJoinHuntButton().should('exist');
+  //   page.getJoinHuntButton().should();
+  // });
+});
--- client/cypress/support/join-hunt.po.ts
+++ client/cypress/support/join-hunt.po.ts
@@ -1,8 +1,8 @@
 export class JoinHuntPage {
-  private readonly baseUrl = '/join-hunt';
-  private readonly pageTitle = '.join-hunt-title';
-  private readonly joinHuntButtonSelector = '[data-test=joinHuntButton]';
-
+  private readonly baseUrl = '/hunters';
+  private readonly pageTitle = '.title';
+  private readonly joinHuntButtonSelector = '[name="join-button"]';
+  private readonly accessCodeInputSelector = '.access-code-inputs';
   navigateTo() {
     return cy.visit(this.baseUrl);
   }
@@ -14,5 +14,23 @@ export class JoinHuntPage {
    */
   getJoinHuntTitle() {
     return cy.get(this.pageTitle);
-}
+  }
+
+/**
+ * Get the join-hunt button DOM element.
+ *
+ * @returns an iterable (`Cypress.Chainable`) containing the `join-hunt` DOM element.
+ */
+  getJoinHuntButton() {
+    return cy.get(this.joinHuntButtonSelector);
+  }
+
+  /**
+   * Get the access code input field DOM element.
+   *
+   * @returns an iterable (`Cypress.Chainable`) containing the `access-code-input` DOM element.
+   */
+  getAccessCodeInput() {
+    return cy.get(this.accessCodeInputSelector);
+  }
 }

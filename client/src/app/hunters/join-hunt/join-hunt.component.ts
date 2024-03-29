e7817fac49fed8e6bb2fec947f7ca3e7e49f6feb
Andrew Lam <lam00139@morris.umn.edu>
2024-03-29 04:22:11 -0500

    Remove Otp method, add onKeyUp, down for moving between box, add checkAccessCode.

--- client/src/app/hunters/join-hunt/join-hunt.component.ts
+++ client/src/app/hunters/join-hunt/join-hunt.component.ts
@@ -1,12 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { RouterModule } from '@angular/router';
-//import { ReactiveFormsModule } from '@angular/forms';
-import { NgxOtpInputModule } from 'ngx-otp-input';
-import { NgxOtpInputConfig } from 'ngx-otp-input';
 import { HostService } from 'src/app/hosts/host.service';
 import { Router } from '@angular/router';
 import { MatSnackBar } from '@angular/material/snack-bar';
@@ -15,7 +12,6 @@ import { MatSnackBar } from '@angular/material/snack-bar';
   selector: 'app-join-hunt',
   standalone: true,
   imports: [
-    NgxOtpInputModule,
     MatInputModule,
     MatCardModule,
     CommonModule,
@@ -30,23 +26,44 @@ export class JoinHuntComponent {
   isAccessCodeValid = false;
 
   accessCode: string;
-errorMessage: string;
-  otpInputConfig:NgxOtpInputConfig = {
-    otpLength: 6,
-    autofocus: true,
-    classList: {
-      inputBox: 'my-super-box-class',
-      input: 'my-super-class',
-      inputFilled: 'my-super-filled-class',
-      inputDisabled: 'my-super-disabled-class',
-      inputSuccess: 'my-super-success-class',
-      inputError: 'my-super-error-class'
-    }
-  };
+  errorMessage: string;
 
   constructor(private hostService: HostService, private router: Router, private snackBar: MatSnackBar) { }
-  handleOtpChange(value:string[]) : void {
-    this.accessCode = value.join('');
+
+  @ViewChild('input1') input1: ElementRef;
+  @ViewChild('input2') input2: ElementRef;
+  @ViewChild('input3') input3: ElementRef;
+  @ViewChild('input4') input4: ElementRef;
+  @ViewChild('input5') input5: ElementRef;
+  @ViewChild('input6') input6: ElementRef;
+
+  onKeyUp(event, inputNumber) {
+    if (event.target.value.length) {
+      switch (inputNumber) {
+        case 1: this.input2.nativeElement.focus(); break;
+        case 2: this.input3.nativeElement.focus(); break;
+        case 3: this.input4.nativeElement.focus(); break;
+        case 4: this.input5.nativeElement.focus(); break;
+        case 5: this.input6.nativeElement.focus(); break;
+      }
+      this.checkAccessCode();
+    }
+  }
+
+  onKeyDown(event, inputNumber) {
+    if (event.key === 'Backspace' && !event.target.value.length) {
+      switch (inputNumber) {
+        case 2: this.input1.nativeElement.focus(); break;
+        case 3: this.input2.nativeElement.focus(); break;
+        case 4: this.input3.nativeElement.focus(); break;
+        case 5: this.input4.nativeElement.focus(); break;
+        case 6: this.input5.nativeElement.focus(); break;
+      }
+    }
+  }
+
+  checkAccessCode() {
+    this.accessCode = [this.input1.nativeElement.value, this.input2.nativeElement.value, this.input3.nativeElement.value, this.input4.nativeElement.value, this.input5.nativeElement.value, this.input6.nativeElement.value].join('');
     if (this.accessCode.length === 6) {
       this.hostService.getStartedHunt(this.accessCode).subscribe({
         next: () => {

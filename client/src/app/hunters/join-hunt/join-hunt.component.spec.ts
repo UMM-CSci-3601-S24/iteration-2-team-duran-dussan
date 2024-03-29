ebf3466be8decec498b951c20946c8ccb797b1ec
Andrew Lam <lam00139@morris.umn.edu>
2024-03-29 04:24:47 -0500

    Adjusting test and add new test for hunters 100% coverage.

--- client/src/app/hunters/join-hunt/join-hunt.component.spec.ts
+++ client/src/app/hunters/join-hunt/join-hunt.component.spec.ts
@@ -22,31 +22,88 @@ describe('JoinHuntComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should handle OTP change with invalid access code', () => {
-    const otp = ['1', '2', '3', '4', '5', '6'];
+  it('should handle change with invalid access code', () => {
+    component.input1 = { nativeElement: { value: '1' } };
+    component.input2 = { nativeElement: { value: '2' } };
+    component.input3 = { nativeElement: { value: '3' } };
+    component.input4 = { nativeElement: { value: '4' } };
+    component.input5 = { nativeElement: { value: '5' } };
+    component.input6 = { nativeElement: { value: '6' } };
     hostService.getStartedHunt.and.returnValue(throwError(new Error()));
-    component.handleOtpChange(otp);
+    component.checkAccessCode();
     expect(component.accessCode).toEqual('123456');
     expect(component.isAccessCodeValid).toEqual(false);
     expect(snackBar.open).toHaveBeenCalledWith('Invalid access code. No hunt was found.', 'Close', { duration: 6000 });
   });
 
-  it('should handle OTP change with invalid length', () => {
-    const otp = ['1', '2', '3'];
-    component.handleOtpChange(otp);
-    expect(component.accessCode).toEqual('123');
-    expect(component.isAccessCodeValid).toEqual(false);
-  });
-
-  it('should handle OTP change with valid access code', () => {
-    const otp = ['1', '2', '3', '4', '5', '6'];
+  it('should handle change with valid access code', () => {
+    component.input1 = { nativeElement: { value: '1' } };
+    component.input2 = { nativeElement: { value: '2' } };
+    component.input3 = { nativeElement: { value: '3' } };
+    component.input4 = { nativeElement: { value: '4' } };
+    component.input5 = { nativeElement: { value: '5' } };
+    component.input6 = { nativeElement: { value: '6' } };
     const startedHunt: StartedHunt = {
       accessCode: '123456', completeHunt: null,
       _id: ''
     };
     hostService.getStartedHunt.and.returnValue(of(startedHunt));
-    component.handleOtpChange(otp);
+    component.checkAccessCode();
+    expect(component.accessCode).toEqual('123456');
     expect(component.isAccessCodeValid).toEqual(true);
+  })
+
+  it('should handle type access code will move to next box', () => {
+    component.input1 = { nativeElement: { focus: jasmine.createSpy() } };
+    component.input2 = { nativeElement: { focus: jasmine.createSpy() } };
+    component.input3 = { nativeElement: { focus: jasmine.createSpy() } };
+    component.input4 = { nativeElement: { focus: jasmine.createSpy() } };
+    component.input5 = { nativeElement: { focus: jasmine.createSpy() } };
+    component.input6 = { nativeElement: { focus: jasmine.createSpy() } };
+
+    component.onKeyUp({ target: { value: '1' } }, 1);
+    expect(component.input2.nativeElement.focus).toHaveBeenCalled();
+    component.onKeyUp({ target: { value: '1' } }, 2);
+    expect(component.input3.nativeElement.focus).toHaveBeenCalled();
+    component.onKeyUp({ target: { value: '1' } }, 3);
+    expect(component.input4.nativeElement.focus).toHaveBeenCalled();
+    component.onKeyUp({ target: { value: '1' } }, 4);
+    expect(component.input5.nativeElement.focus).toHaveBeenCalled();
+    component.onKeyUp({ target: { value: '1' } }, 5);
+    expect(component.input6.nativeElement.focus).toHaveBeenCalled();
   });
 
+  it('should handle delete access code will move to next box', () => {
+    component.input1 = { nativeElement: { focus: jasmine.createSpy() } };
+    component.input2 = { nativeElement: { focus: jasmine.createSpy() } };
+    component.input3 = { nativeElement: { focus: jasmine.createSpy() } };
+    component.input4 = { nativeElement: { focus: jasmine.createSpy() } };
+    component.input5 = { nativeElement: { focus: jasmine.createSpy() } };
+    component.input6 = { nativeElement: { focus: jasmine.createSpy() } };
+
+    component.onKeyUp({ target: { value: '1' } }, 1);
+    expect(component.input2.nativeElement.focus).toHaveBeenCalled();
+    component.onKeyDown({ key: 'Backspace', target: { value: '' } }, 2);
+    expect(component.input1.nativeElement.focus).toHaveBeenCalled();
+
+    component.onKeyUp({ target: { value: '2' } }, 2);
+    expect(component.input3.nativeElement.focus).toHaveBeenCalled();
+    component.onKeyDown({ key: 'Backspace', target: { value: '' } }, 3);
+    expect(component.input2.nativeElement.focus).toHaveBeenCalled();
+
+    component.onKeyUp({ target: { value: '3' } }, 3);
+    expect(component.input4.nativeElement.focus).toHaveBeenCalled();
+    component.onKeyDown({ key: 'Backspace', target: { value: '' } }, 4);
+    expect(component.input3.nativeElement.focus).toHaveBeenCalled();
+
+    component.onKeyUp({ target: { value: '4' } }, 4);
+    expect(component.input5.nativeElement.focus).toHaveBeenCalled();
+    component.onKeyDown({ key: 'Backspace', target: { value: '' } }, 5);
+    expect(component.input4.nativeElement.focus).toHaveBeenCalled();
+
+    component.onKeyUp({ target: { value: '5' } }, 5);
+    expect(component.input6.nativeElement.focus).toHaveBeenCalled();
+    component.onKeyDown({ key: 'Backspace', target: { value: '' } }, 6);
+    expect(component.input5.nativeElement.focus).toHaveBeenCalled();
+  })
 });

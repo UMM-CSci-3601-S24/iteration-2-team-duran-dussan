8d1101a40a03294f7a19eb028826ff9ec40d79bf
Andrew Lam <lam00139@morris.umn.edu>
2024-04-01 09:18:45 -0500

    Try to fix failing test in hunt-profile.cy.ts

--- client/cypress/e2e/hunt-profile.cy.ts
+++ client/cypress/e2e/hunt-profile.cy.ts
@@ -40,7 +40,7 @@ describe('Hunt Profile', () => {
     page.getHuntCardBeginHuntButton().click();
     page.getAccessCode().then((accessCode) => {
       cy.wait(1000);
-      cy.url().should('match', new RegExp(`/startedHunts/${accessCode}$`));
+      cy.url().should('eq', `http://localhost:4200/startedHunts/${accessCode}`);
     });
   })
 

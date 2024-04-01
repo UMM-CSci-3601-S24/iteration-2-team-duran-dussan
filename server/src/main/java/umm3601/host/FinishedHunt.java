a5e576ebcfa0c53a6f7c1247512cfede3eddab4d
Ely Johnson <elyjohnson6@gmail.com>
2024-04-01 18:44:16 -0500

    Created FinishedHunt and FinishedTask classes

---
+++ server/src/main/java/umm3601/host/FinishedHunt.java
@@ -0,0 +1,9 @@
+package umm3601.host;
+
+import java.util.ArrayList;
+
+@SuppressWarnings({"VisibilityModifier"})
+public class FinishedHunt {
+  public StartedHunt startedHunt;
+  public ArrayList<FinishedTask> tasks;
+}
---
+++ server/src/main/java/umm3601/host/FinishedTask.java
@@ -0,0 +1,9 @@
+package umm3601.host;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class FinishedTask {
+  Task task;
+  ArrayList<File> photos;
+}

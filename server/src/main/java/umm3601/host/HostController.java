68c9a29ce1a465b10c0efc8068face5cef8b7761
Ely Johnson <elyjohnson6@gmail.com>
2024-04-02 14:04:26 -0500

    Changed finishedHunts to endedHunts for getEndedHunts API url

--- server/src/main/java/umm3601/host/HostController.java
+++ server/src/main/java/umm3601/host/HostController.java
@@ -45,7 +45,7 @@ public class HostController implements Controller {
   private static final String API_START_HUNT = "/api/startHunt/{id}";
   private static final String API_STARTED_HUNT = "/api/startedHunts/{accessCode}";
   private static final String API_END_HUNT = "/api/endHunt/{id}";
-  private static final String API_ENDED_HUNT = "/api/finishedHunts/{id}";
+  private static final String API_ENDED_HUNT = "/api/endedHunts/{id}";
   private static final String API_ENDED_HUNTS = "/api/hosts/{id}/endedHunts";
   private static final String API_DELETE_HUNT = "/api/endedHunts/{id}";
   private static final String API_PHOTO_UPLOAD = "/api/startedHunt/{startedHuntId}/tasks/{taskId}/photo";

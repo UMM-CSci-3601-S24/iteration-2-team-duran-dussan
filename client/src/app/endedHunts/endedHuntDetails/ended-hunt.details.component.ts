83e80f443eb3808f724dd4730f4b847c3f63997e
Ely Johnson <elyjohnson6@gmail.com>
2024-04-02 14:22:55 -0500

    Added export class EndedHuntDetailsComponent that makes a call to the server for an ended hunt

---
+++ client/src/app/endedHunts/endedHuntDetails/ended-hunt.details.component.ts
@@ -0,0 +1,59 @@
+import { HttpClientModule } from "@angular/common/http";
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { MatIconButton } from "@angular/material/button";
+import { MatCardModule } from "@angular/material/card";
+import { MatDivider } from "@angular/material/divider";
+import { MatIcon } from "@angular/material/icon";
+import { AddTaskComponent } from "src/app/hunts/addTask/add-task.component";
+import { EndedHuntCardComponent } from "../ended-hunt-card.component";
+import { EndedHunt } from "../endedHunt";
+import { Subject, map, switchMap, takeUntil } from "rxjs";
+import { MatDialog } from "@angular/material/dialog";
+import { MatSnackBar } from "@angular/material/snack-bar";
+import { ActivatedRoute, ParamMap, Router } from "@angular/router";
+import { HostService } from "src/app/hosts/host.service";
+
+@Component({
+  selector: 'app-ended-hunt-details',
+  templateUrl: './ended-hunt-details.component.html',
+  styleUrls: ['./ended-hunt-details.component.scss'],
+  standalone: true,
+  imports: [EndedHuntCardComponent, MatCardModule, AddTaskComponent, MatDivider, MatIconButton, MatIcon, HttpClientModule]
+})
+export class EndedHuntDetailsComponent implements OnInit, OnDestroy{
+  confirmDeleteHunt: boolean = false;
+  endedHunt: EndedHunt;
+  error: { help: string, httpResponse: string, message: string };
+
+  private ngUnsubscribe = new Subject<void>();
+
+  constructor(private snackBar: MatSnackBar, private route: ActivatedRoute, private hostService: HostService, private router: Router, public dialog: MatDialog) { }
+
+  ngOnInit(): void {
+    this.route.paramMap.pipe(
+
+      map((paramMap: ParamMap) => paramMap.get('id')),
+
+      switchMap((id: string) => this.hostService.getEndedHuntById(id)),
+
+      takeUntil(this.ngUnsubscribe)
+    ).subscribe({
+      next: endedHunt => {
+        this.endedHunt = endedHunt;
+        return ;
+      },
+      error: _err => {
+        this.error = {
+          help: 'There was a problem loading the endedHunt – try again.',
+          httpResponse: _err.message,
+          message: _err.error?.title,
+        };
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.ngUnsubscribe.next();
+    this.ngUnsubscribe.complete();
+  }
+}

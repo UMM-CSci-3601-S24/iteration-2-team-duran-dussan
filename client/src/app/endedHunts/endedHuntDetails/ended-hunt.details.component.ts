4cdf4206a3e0619ae063db35c583a269a67178f1
Ely Johnson <elyjohnson6@gmail.com>
2024-04-04 10:24:15 -0500

    Added return to hunts button to ended hunt details page

--- client/src/app/endedHunts/endedHuntDetails/ended-hunt.details.component.ts
+++ client/src/app/endedHunts/endedHuntDetails/ended-hunt.details.component.ts
@@ -1,60 +1,77 @@
-import { HttpClientModule } from "@angular/common/http";
-import { Component, OnDestroy, OnInit } from "@angular/core";
-import { MatIconButton } from "@angular/material/button";
-import { MatCardModule } from "@angular/material/card";
-import { MatDivider } from "@angular/material/divider";
-import { MatIcon } from "@angular/material/icon";
-import { AddTaskComponent } from "src/app/hunts/addTask/add-task.component";
-import { EndedHuntCardComponent } from "../ended-hunt-card.component";
-import { EndedHunt } from "../endedHunt";
-import { Subject, map, switchMap, takeUntil } from "rxjs";
-import { MatDialog } from "@angular/material/dialog";
-import { ActivatedRoute, ParamMap, Router } from "@angular/router";
-import { HostService } from "src/app/hosts/host.service";
-import { CommonModule } from "@angular/common";
+import { HttpClientModule } from '@angular/common/http';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { MatIconButton } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDivider } from '@angular/material/divider';
+import { MatIcon } from '@angular/material/icon';
+import { AddTaskComponent } from 'src/app/hunts/addTask/add-task.component';
+import { EndedHuntCardComponent } from '../ended-hunt-card.component';
+import { EndedHunt } from '../endedHunt';
+import { Subject, map, switchMap, takeUntil } from 'rxjs';
+import { MatDialog } from '@angular/material/dialog';
+import { ActivatedRoute, ParamMap, Router, RouterLink } from '@angular/router';
+import { HostService } from 'src/app/hosts/host.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-ended-hunt-details',
   templateUrl: './ended-hunt-details.component.html',
   styleUrls: ['./ended-hunt-details.component.scss'],
   standalone: true,
-  imports: [EndedHuntCardComponent, MatCardModule, AddTaskComponent, MatDivider, MatIconButton, MatIcon, HttpClientModule, CommonModule]
+  imports: [
+    EndedHuntCardComponent,
+    MatCardModule,
+    AddTaskComponent,
+    MatDivider,
+    MatIconButton,
+    MatIcon,
+    HttpClientModule,
+    CommonModule,
+    RouterLink
+  ],
 })
-export class EndedHuntDetailsComponent implements OnInit, OnDestroy{
+export class EndedHuntDetailsComponent implements OnInit, OnDestroy {
   confirmDeleteHunt: boolean = false;
   endedHunt: EndedHunt;
-  error: { help: string, httpResponse: string, message: string };
+  error: { help: string; httpResponse: string; message: string };
 
   private ngUnsubscribe = new Subject<void>();
 
-  constructor(private route: ActivatedRoute, private hostService: HostService, private router: Router, public dialog: MatDialog) { }
+  constructor(
+    private route: ActivatedRoute,
+    private hostService: HostService,
+    private router: Router,
+    public dialog: MatDialog
+  ) {}
 
   ngOnInit(): void {
+    this.route.paramMap
+      .pipe(
+        map((paramMap: ParamMap) => paramMap.get('id')),
 
-    this.route.paramMap.pipe(
+        switchMap((id: string) => this.hostService.getEndedHuntById(id)),
 
-      map((paramMap: ParamMap) => paramMap.get('id')),
-
-      switchMap((id: string) => this.hostService.getEndedHuntById(id)),
-
-      takeUntil(this.ngUnsubscribe)
-    ).subscribe({
-      next: endedHunt => {
-        this.endedHunt = endedHunt;
-        return ;
-      },
-      error: _err => {
-        this.error = {
-          help: 'There was a problem loading the endedHunt – try again.',
-          httpResponse: _err.message,
-          message: _err.error?.title,
-        };
-      }
-    });
+        takeUntil(this.ngUnsubscribe)
+      )
+      .subscribe({
+        next: (endedHunt) => {
+          this.endedHunt = endedHunt;
+          return;
+        },
+        error: (_err) => {
+          this.error = {
+            help: 'There was a problem loading the endedHunt – try again.',
+            httpResponse: _err.message,
+            message: _err.error?.title,
+          };
+        },
+      });
   }
 
   getTaskName(taskId: string): string {
-    return this.endedHunt.startedHunt.completeHunt.tasks.find(task => task._id === taskId).name;
+    return this.endedHunt.startedHunt.completeHunt.tasks.find(
+      (task) => task._id === taskId
+    ).name;
   }
 
   ngOnDestroy(): void {
